02ddbaf6411090d284fddf64d97f64a514844160
Thomas Sifuentes <thomas.sifuentes@verizon.com>
2015-05-11 21:08:21 -0500

    'mensaje'

--- server.js
+++ server.js
@@ -1,43 +1,47 @@
-var Server = require('ws').Server,
-    wss = new Server({
+var Server = require('ws').Server, number = 0;
+GLOBAL.wss = new Server({
         port: 8000
     });
 
-var currentWS = false;
-var notifications = {
+// var currentWS = false;
+GLOBAL.notifications = {
 
     users: {},
 
     init: function() {
         wss.on('connection', function(ws) {
             console.log('session started');
-            currentWS = ws;
+            // currentWS = ws;
             ws.on('message', function(data) {
-                currentWS = ws;
+                // currentWS = ws;
+                
             });
 
-            ws.on('close', function() {
-                currentWS = false;
-                console.log('session close');
-            });
+            // ws.on('close', function() {
+            //     currentWS = false;
+            //     console.log('session close');
+            // });
         });
     },
-    notify: function(number) {
+    notify: function(number, user) {
         var data ={};
         data.key ="1";
         var notification = {module: "BILLING", number: "INQ2014234234234", by: "for Account Number 322335", status: "is Working: Under Investigation", seen: true} ;
         notification.module = notification.module + number;
         data.data = notification;
-        if (currentWS) {
+
+        if (user) {
             console.log('nofity');
-            currentWS.send(JSON.stringify(data));
+            user.send(JSON.stringify(data));
         }
     }
 
 };
-var number =0;
+
 notifications.init();
 console.log('started');
 setInterval(function() {
-    notifications.notify(number++);
-}, 6000);
+    var user = parseInt(Math.random()*100000) % wss.clients.length;
+    notifications.notify(number++, wss.clients[user]);
+    // notifications.notify(number++);
+}, 2000);
